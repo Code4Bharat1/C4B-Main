819a935a01d7eeae6649b8b5f931968fe6d2310d
ParasVarankar23 <parasvarankar235@gmail.com>
2025-04-18 19:06:58 -0700

    changes by paras

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,33 @@
 import "./globals.css";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Code4Bharat",
-  description: "Code4Bharat",
+  description: "Code4Bharat",  // Fixed typo here
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="non-selective">{children}</body>
+      <head>
+        {/*Google Search Console*/}
+        
+        <meta name="google-site-verification" content="FpT8vyz4FajVkRmympX15FZ4jlefoZyxS20-dq-Fpy0" />
+        {/* Google Analytics Code */}
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2DMPGE1KGS"></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2DMPGE1KGS');
+          `}
+        </Script>
+      </head>
+      <body className="non-selective">
+        {children}
+      </body>
     </html>
   );
 }
